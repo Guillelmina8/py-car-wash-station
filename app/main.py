7cd027d1ef49e40d6fbec3e8a76c1490ca5bd1f6
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-10-27 22:18:00 +0200

    improved code style and logic moments

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
-from typing import List
-
-
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -25,14 +27,14 @@ class CarWashStation:
         return round(income, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: List[Car]) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                price = self.calculate_washing_price(car)
-                income += price
+                income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         total_income = round(income, 1)
         return total_income
