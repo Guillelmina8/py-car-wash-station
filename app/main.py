f1be82346213d87f6fe577d24e4369aeb29da579
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-10-27 17:18:45 +0200

    Corrected flake8 errors

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
@@ -17,10 +18,10 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        income = (car.comfort_class *
-                  (self.clean_power - car.clean_mark) *
-                  self.average_rating /
-                  self.distance_from_city_center)
+        income = (car.comfort_class
+                  * (self.clean_power - car.clean_mark)
+                  * self.average_rating
+                  / self.distance_from_city_center)
         return round(income, 1)
 
     def wash_single_car(self, car: Car) -> None:
